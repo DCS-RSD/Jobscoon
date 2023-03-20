fb48d64c997c1dc797039b5dbdb286a171e51b95
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2023-03-20 22:17:36 +0800

    sign up button

--- app/src/main/java/com/example/assignment/LoginActivity.kt
+++ app/src/main/java/com/example/assignment/LoginActivity.kt
@@ -3,12 +3,18 @@ package com.example.assignment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.View
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
 
     fun signUp(v:View){
