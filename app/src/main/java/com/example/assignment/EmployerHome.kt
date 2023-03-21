59717e2ee395066219db5db8a49a850262958ed8
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2023-03-21 15:23:45 +0800

    employer home

---
+++ app/src/main/java/com/example/assignment/EmployerHome.kt
@@ -0,0 +1,14 @@
+package com.example.assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ScrollView
+import androidx.cardview.widget.CardView
+
+class EmployerHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_employer_home)
+    }
+}
--- app/src/main/java/com/example/assignment/EmployerSignUpActivity.kt
+++ app/src/main/java/com/example/assignment/EmployerSignUpActivity.kt
@@ -1,14 +1,21 @@
 package com.example.assignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 
 class EmployerSignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_employer_sign_up)
+
+        val btn = findViewById<Button>(R.id.sign_up_btn)
+        btn.setOnClickListener{
+            startActivity(Intent(this,EmployerHome::class.java))
+        }
 
     }
 }
