1ec8a91deb206258b1e1f53e5a2607f7e56ba7e7
GohChunYen <chunyengoh030503@gmail.com>
2023-03-22 14:01:56 +0800

    sf
    sdf

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.job_details)
+        setContentView(R.layout.my_jobs)
     }
 
 }
