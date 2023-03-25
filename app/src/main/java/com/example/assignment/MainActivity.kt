7f2255f04239906a5cd44fc36e8c7246008dcaaa
GohChunYen <chunyengoh030503@gmail.com>
2023-03-25 23:34:20 +0800

    25/3/2023
    editprofile_employee
    home
    colors
    styles
    mainactivity

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -9,13 +9,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.editprofile_employee)
 
+        /*
         val btn = findViewById<Button>(R.id.employer_login_btn)
         btn.setOnClickListener{
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+        */
+
     }
 }
 
