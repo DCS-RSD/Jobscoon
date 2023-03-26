42f15e4af7a2552ac08cacede943e1987d7dd1e5
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2023-03-26 12:52:17 +0800

    done interview schedule

--- app/src/main/java/com/example/assignment/InterviewFormActivity.kt
+++ app/src/main/java/com/example/assignment/InterviewFormActivity.kt
@@ -1,33 +1,11 @@
 package com.example.assignment
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
 
-//to be delete
-class InterviewFormLayout : AppCompatActivity() {
+class InterviewFormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_interview_form_layout)
-//        val typeSpinner = findViewById<Spinner>(R.id.type_spinner)
-//        val interviewType = arrayOf("Physical", "Virtual")
-
-//        ArrayAdapter.createFromResource(
-//            this,
-//            R.array.interview_type,
-//            android.R.layout.simple_spinner_item
-//        )
-//            .also { adapter ->
-//                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//                typeSpinner.adapter = adapter
-//            }
-
-        val confirmBtn = findViewById<Button>(R.id.confirm_interview_btn)
-        confirmBtn.setOnClickListener {
-            startActivity(Intent(this, UpcomingInterviewActivity::class.java))
-        }
+        setContentView(R.layout.activity_interview_form)
     }
 }
