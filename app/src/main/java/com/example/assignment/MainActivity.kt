571fca549d66a03cf757730df0484df3523e8a37
GohChunYen <chunyengoh030503@gmail.com>
2023-03-26 10:18:36 +0800

    26/3/2023
    activity_job_post_form
    activity_employee_career_development
    styles

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_job_post_form)
+        setContentView(R.layout.activity_employee_career_development)
 
         /*
         val btn = findViewById<Button>(R.id.employer_login_btn)
