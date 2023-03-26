9fbd16456fe6de866590b80f4507f50c672ab50f
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2023-03-26 12:21:52 +0800

    remove duplicate interview form

--- app/src/main/java/com/example/assignment/InterviewFormActivity.kt
+++ app/src/main/java/com/example/assignment/InterviewFormActivity.kt
@@ -1,23 +1,33 @@
 package com.example.assignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 
-class InterviewFormActivity : AppCompatActivity() {
+//to be delete
+class InterviewFormLayout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_interview_form)
+        setContentView(R.layout.activity_interview_form_layout)
+//        val typeSpinner = findViewById<Spinner>(R.id.type_spinner)
+//        val interviewType = arrayOf("Physical", "Virtual")
 
-        val typeSpinner = findViewById<Spinner>(R.id.type_spinner)
-        val interviewType = arrayOf("Physical","Virtual")
+//        ArrayAdapter.createFromResource(
+//            this,
+//            R.array.interview_type,
+//            android.R.layout.simple_spinner_item
+//        )
+//            .also { adapter ->
+//                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//                typeSpinner.adapter = adapter
+//            }
 
-        ArrayAdapter.createFromResource(this,R.array.interview_type,android.R.layout.simple_spinner_item)
-            .also {
-                adapter->
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                typeSpinner.adapter = adapter
-            }
+        val confirmBtn = findViewById<Button>(R.id.confirm_interview_btn)
+        confirmBtn.setOnClickListener {
+            startActivity(Intent(this, UpcomingInterviewActivity::class.java))
+        }
     }
 }
