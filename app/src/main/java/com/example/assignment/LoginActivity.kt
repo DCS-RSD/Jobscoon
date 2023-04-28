3bb2dd8573e7eb180bbf3762f24813c75894148c
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2023-04-28 17:03:59 +0800

    e

--- app/src/main/java/com/example/assignment/LoginActivity.kt
+++ app/src/main/java/com/example/assignment/LoginActivity.kt
@@ -13,7 +13,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.Navigation
-import com.example.assignment.auth.LoginErrorResponse
+
 import com.example.assignment.auth.LoginResponse
 import com.example.assignment.api.RetrofitBuild
 import com.example.assignment.databinding.ActivityLoginBinding
@@ -49,68 +49,6 @@ class LoginActivity : AppCompatActivity() {
 
 
 
-    private fun submitLogin() {
-
-        val build = RetrofitBuild.build()
-            .login(
-                binding.loginEmail.text.toString(),
-                binding.loginPassword.text.toString(),
-            )
-
-        build.enqueue(object : Callback<LoginResponse?> {
-            override fun onResponse(
-                call: Call<LoginResponse?>,
-                response: Response<LoginResponse?>
-            ) {
-                if (response.isSuccessful) {
-                    Toast.makeText(applicationContext, "login success", Toast.LENGTH_SHORT).show()
-                    startActivity(Intent(this@LoginActivity, EmployerHomeActivity::class.java))
-
-                    val sharedPreferences = getSharedPreferences("User", Context.MODE_PRIVATE)
-                    val editor = sharedPreferences.edit()
-                    editor.putString("Token","Bearer " + response.body()!!.token)
-                    editor.apply()
-                    finish()
-
-                } else if (response.code() == 422) { //validation fails
-                    val error = Gson().fromJson(
-                        response.errorBody()!!.string(),
-                        ValidationErrorResponse::class.java
-                    )
-
-                    Toast.makeText(
-                        applicationContext,
-                        "Login Fail : " + error.message,
-                        Toast.LENGTH_SHORT
-                    ).show()
-
-                    Log.d("login", "onResponse: " + error.errors)
-                } else if (response.code() == 401) { //wrong email or password
-                    Toast.makeText(
-                        applicationContext,
-                        "Invalid Email or Password",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                } else { //unknown error
-                    Toast.makeText(
-                        applicationContext,
-                        "Something went wrong",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-
-            override fun onFailure(call: Call<LoginResponse?>, t: Throwable) {
-                Log.d("fail", "onFailure: " + t.message)
-
-                Toast.makeText( //no connection
-                    applicationContext,
-                    "Something went wrong. Kindly check your connection",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        })
-    }
 
 
 }
