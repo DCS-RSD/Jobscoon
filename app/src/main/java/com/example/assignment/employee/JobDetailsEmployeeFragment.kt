b06d367a0af8145dfb2ba130b1eda3d97f002b27
GohChunYen <chunyengoh030503@gmail.com>
2023-05-02 23:07:53 +0800

    temp

--- app/src/main/java/com/example/assignment/employee/JobDetailsEmployeeFragment.kt
+++ app/src/main/java/com/example/assignment/employee/JobDetailsEmployeeFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
@@ -34,7 +37,10 @@ class JobDetailsEmployeeFragment : Fragment() {
 
         binding.imageView.setOnClickListener {
                 view : View -> view.findNavController().popBackStack()
+        }
 
+        binding.applyButton.setOnClickListener{
+            showAlert()
         }
 
         return binding.root
@@ -46,6 +52,23 @@ class JobDetailsEmployeeFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(JobDetailsEmployeeViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    fun showAlert() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Apply Job")
+        builder.setMessage("Are you sure to apply this job ?")
+        builder.setPositiveButton("Yes") { dialog, which ->
+            Toast.makeText(requireContext(), "You have applied the job !", Toast.LENGTH_LONG).show()
+            //TODO: backend, disableApplyButtonOnCreate, changeToDisableColor
+            binding.applyButton.isEnabled = false
+            binding.applyButton.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.disabled_button_color)
+
+        }
+        builder.setNegativeButton("No") { dialog, which ->
+            dialog.dismiss()
+        }
+        builder.show()
     }
 
 }
