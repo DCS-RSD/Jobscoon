8a114a579f65da344776913f91bed1ac831c050a
GohChunYen <chunyengoh030503@gmail.com>
2023-05-10 00:48:34 +0800

    temp

--- app/src/main/java/com/example/assignment/employee/CareerDevelopmentDetailsFragment.kt
+++ app/src/main/java/com/example/assignment/employee/CareerDevelopmentDetailsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.assignment
+package com.example.assignment.employee
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.assignment.CareerDevelopmentDetailsViewModel
+import com.example.assignment.R
 
 class CareerDevelopmentDetailsFragment : Fragment() {
 
