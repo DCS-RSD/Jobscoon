b17c7decaea40beddcd31977f180d979e05c0ff2
GohChunYen <chunyengoh030503@gmail.com>
2023-05-12 19:37:28 +0800

    temp

--- app/src/main/java/com/example/assignment/employer/ApplicantListEmployerFragment.kt
+++ app/src/main/java/com/example/assignment/employer/ApplicantListEmployerFragment.kt
@@ -1,13 +1,12 @@
-package com.example.assignment
+package com.example.assignment.employer
 
-import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import com.example.assignment.R
 
 class ApplicantListEmployerFragment : Fragment() {
 
--- app/src/main/java/com/example/assignment/employer/ApplicantListEmployerViewModel.kt
+++ app/src/main/java/com/example/assignment/employer/ApplicantListEmployerViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.assignment
+package com.example.assignment.employer
 
 import androidx.lifecycle.ViewModel
 
