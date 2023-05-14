313fa495118f88dfef745ecdc19df6db8d106e99
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2023-05-14 20:06:07 +0800

    sohai navigation

--- app/src/main/java/com/example/assignment/employee/ProfileEmployeeFragment.kt
+++ app/src/main/java/com/example/assignment/employee/ProfileEmployeeFragment.kt
@@ -4,22 +4,16 @@ import android.content.Context
 import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import com.example.assignment.ProfileEmployeeViewModel
 import com.example.assignment.R
 import com.example.assignment.auth.AuthActivity
-import com.example.assignment.auth.LoginViewModel
 import com.example.assignment.databinding.FragmentProfileEmployeeBinding
 
 class ProfileEmployeeFragment : Fragment() {
---
+++ app/src/main/java/com/example/assignment/employer/InterviewEmployerFragment.kt
@@ -0,0 +1,32 @@
+package com.example.assignment.employer
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.assignment.R
+
+class InterviewEmployerFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = InterviewEmployerFragment()
+    }
+
+    private lateinit var viewModel: InterviewEmployerViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_interview_employer, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(InterviewEmployerViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
